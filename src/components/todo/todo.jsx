0ca32f431a811b8a390c3048882e271aa13222f2
Chaya <ctanenbu@student.touro.edu>
2023-02-10 11:25:53 -0500

    restructured files

---
+++ src/components/todo/todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Todo = () => {
+    return(
+        <>
+        <h1>Todo</h1>
+        <p>cook for Shabbos</p>
+        <p>finish homework</p>
+        <p>hang out with friends</p>
+        </>
+        
+    )
+}
